a0eb1f9cf3d6cf181a3949ba1ac0f32f41539191
Lucas William <59783795+LucasJordao@users.noreply.github.com>
2020-05-30 14:15:22 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/lucaswilliam/workshopmongo/domain/Post.java
@@ -0,0 +1,106 @@
+package com.lucaswilliam.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Document
+public class Post implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	//Attributes
+	@Id
+	private String id;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy'T'HH:mm:ss'Z'", timezone = "GMT")
+	private Instant date;
+	private String title;
+	private String body;
+	private User author;
+	
+	//Constructors and Overloads
+	public Post(){
+		
+	}
+
+	public Post(String id, Instant date, String title, String body, User author) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.title = title;
+		this.body = body;
+		this.author = author;
+	}
+
+	//Getters and Setters
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+	
+	//HashCode and Equals
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/com/lucaswilliam/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.lucaswilliam.workshopmongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucaswilliam.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
