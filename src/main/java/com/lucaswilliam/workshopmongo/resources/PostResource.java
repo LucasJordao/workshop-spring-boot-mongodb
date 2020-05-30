e824111bf751af8af6a05d602f7a7d6f7f70387d
Lucas William <59783795+LucasJordao@users.noreply.github.com>
2020-05-30 16:23:58 -0300

    Consulta simples com query methods

--- src/main/java/com/lucaswilliam/workshopmongo/resources/PostResource.java
+++ src/main/java/com/lucaswilliam/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.lucaswilliam.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucaswilliam.workshopmongo.domain.Post;
+import com.lucaswilliam.workshopmongo.resources.util.URL;
 import com.lucaswilliam.workshopmongo.services.PostService;
 
 @RestController
@@ -23,4 +27,12 @@ public class PostResource {
 		return ResponseEntity.ok().body(post);
 	}
 	
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue= "") String text){
+		text = URL.decodedParam(text);
+		
+		List<Post> list = service.findByTitle(text);
+		
+		return ResponseEntity.ok().body(list);
+	}
 }
--- src/main/java/com/lucaswilliam/workshopmongo/services/PostService.java
+++ src/main/java/com/lucaswilliam/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.lucaswilliam.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,9 @@ public class PostService {
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(id));
 	}
+
 	
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
+	}
 }
