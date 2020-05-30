81fbd15b87388a14882afcd96bc76138af16707b
Lucas William <59783795+LucasJordao@users.noreply.github.com>
2020-05-30 15:02:04 -0300

    Referenciando os posts do usuario

--- src/main/java/com/lucaswilliam/workshopmongo/domain/User.java
+++ src/main/java/com/lucaswilliam/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.lucaswilliam.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -15,6 +18,8 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	//Constructors and Overloads
 	public User() {
@@ -51,6 +56,14 @@ public class User implements Serializable{
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	//HashCode and Equals
