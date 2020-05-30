f8968f939778da37ca1a206912a5ea91cbd2700a
Lucas William <59783795+LucasJordao@users.noreply.github.com>
2020-05-30 15:27:05 -0300

    Obtendo um post por id

---
+++ src/main/java/com/lucaswilliam/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.lucaswilliam.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucaswilliam.workshopmongo.domain.Post;
+import com.lucaswilliam.workshopmongo.repositories.PostRepository;
+import com.lucaswilliam.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+	
+}
