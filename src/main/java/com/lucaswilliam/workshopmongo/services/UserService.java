24634ecc28550228f4f8fd5ac2f3c95da048688a
Lucas William <59783795+LucasJordao@users.noreply.github.com>
2020-05-30 13:06:32 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/lucaswilliam/workshopmongo/services/UserService.java
+++ src/main/java/com/lucaswilliam/workshopmongo/services/UserService.java
@@ -33,6 +33,11 @@ public class UserService {
 		return repository.insert(user);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO obj) {
 		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
